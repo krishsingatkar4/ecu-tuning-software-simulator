f597fc175c0a3aabfef565c516fb507ca4c90e0b
krishsingatkar4 <krishsingatkar4@gmail.com>
2026-07-24 00:05:09 +0530

    Phase 10.2: Added professional dyno performance report

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ manager.create_demo_project()
 print("\n===== DISPLAY TEST =====\n")
 
 manager.display_all_cars()
+
+car = manager.cars[0]
+
+car.ecu.profile.dyno.generate_performance_report(car,car.ecu)
